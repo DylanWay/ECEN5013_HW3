5bdf198df425e7740b68f6a7c5bda5ec585d4c42
DylanWay <dylan.way@colorado.edu>
2016-09-26 21:01:58 -0600

    Create circbuf.h

---
+++ circbuf.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+*
+*  	Filename: circbuf.h
+*	  Description: Header file for circbuf.c
+*                    
+*   Author: Dylan Way
+*       
+****************************************************************************/
+#ifndef _CIRCBUF_H
+#define _CIRCBUF_H
+
+/* Defines section */
+// Circular buffer definition from class lecture
+typedef struct CircBuf {
+    void * buffer;    // Beginning of buffer in memory
+    void * head;      // Pointer to oldest
+    void * tail;      // Pointer to newest
+    size_t size;      // Size of the buffer
+    size_t item_size  // Size of each item (bytes)
+    size_t num_items; // Current number of items
+};
+
+// Enum for the circular buffer states
+enum buffer_states 
+{
+    EMPTY,
+    PARTFILL,
+    FULL
+};
+
+/* Function prototype Section */
+// Checks if the circular buffer is full. Returns 1 if full, otherwise returns 0.
+int8_t buffer_full(CircBuf *cb);
+
+// Checks if the circular buffer is empty. Returns 1 if empty, otherwise returns 0.
+int8_t buffer_empty(CircBuf *cb);
+
+// Adds an item to the circular buffer if the buffer is not full. If full, returns an error.
+int8_t add_buffer_item(CircBuf *cb, void *item);
+
+// Removes an item from the circular buffer if the buffer is not empty. If empty, returns an error.
+int8_t remove_buffer_item(CircBuf *cb, void *item);
+
+#endif
